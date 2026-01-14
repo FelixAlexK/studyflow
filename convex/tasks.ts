8a775d4b94f42f2e2c15f26ec88e51cf43b055d4
Felix Kuhbier <felix.kuhbier@yahoo.com>
2026-01-14 19:31:02 +0100

    style: format code for consistency and readability across multiple files

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,21 +1,22 @@
-import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 // Return the last 100 tasks in a given task list.
 export const getTaskList = query({
-  args: { 
-   },
-  handler: async (ctx, args) => {
-    const tasks = await ctx.db
-      .query("tasks").collect()
-    return tasks;
-  },
+	args: {},
+	handler: async (ctx, _args) => {
+		const tasks = await ctx.db.query("tasks").collect();
+		return tasks;
+	},
 });
 
 export const createTask = mutation({
-  args: { text: v.string() },
-  handler: async (ctx, args) => {
-    const newTaskId = await ctx.db.insert("tasks", { text: args.text, isCompleted: false });
-    return newTaskId;
-  },
+	args: { text: v.string() },
+	handler: async (ctx, args) => {
+		const newTaskId = await ctx.db.insert("tasks", {
+			text: args.text,
+			isCompleted: false,
+		});
+		return newTaskId;
+	},
 });
