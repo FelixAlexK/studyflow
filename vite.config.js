62d1b8878b7bda16b4aec88f9510d13cc6a93f1e
Felix Kuhbier <felix.kuhbier@yahoo.com>
2026-01-14 17:26:24 +0100

    feat: add proxy configuration for authentication API

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+
+	 server: {
+    proxy: {
+      '/api/auth': {
+        target: process.env.VITE_PUBLIC_CONVEX_SITE_URL || 'https://outgoing-ox-416.convex.site',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
