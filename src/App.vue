5c782a1f8a3e6688b5e67b89a3775b0d56dc7ce8
Felix Kuhbier <felix.kuhbier@yahoo.com>
2026-01-14 16:14:01 +0100

    feat: initialize Convex backend

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+<script setup lang="ts">
+import { useConvexQuery, useConvexMutation } from "convex-vue";
+import { api } from "../convex/_generated/api";
+
+const { data, isPending } = useConvexQuery(api.tasks.getTaskList);
+const createTask = useConvexMutation(api.tasks.createTask);
+
+const createTaskHandler = async () => {
+  const text = prompt("Enter task text:");
+  if (text) {
+    await createTask.mutate({ text });
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button @click="createTaskHandler">Add Task</button>
+  <span v-if="isPending"> Loading... </span>
+  <ul v-else>
+    <li v-for="todo in data">
+      {{ todo.text }} {{ todo.isCompleted ? "☑" : "☐" }}
+    </li>
+  </ul>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
